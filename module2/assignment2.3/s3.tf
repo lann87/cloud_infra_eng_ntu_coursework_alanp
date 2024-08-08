92400be490fdff81a013eb7cd7fcc14898cf1c2c
Alan Peh <alanpeh87@gmail.com>
2024-08-08 18:49:33 +0800

    Submission of coursework without markdown files yet

--- module2/assignment2.3/s3.tf
+++ module2/assignment2.3/s3.tf
@@ -1,18 +1,30 @@
 resource "aws_s3_bucket" "alanp_s3_tf_deployment" {
   bucket = "alanp-s3-a3-donotdelete"
-  acl = "private" // Set to private to avoid charges
+}
 
-  # Enabled versioning to track version of object in same bucket
-  versioning {
-    enabled = true
-  }
+# ACL Config
+#resource "aws_s3_bucket_acl" "alanp_s3_tf_acl" {
+#  bucket = aws_s3_bucket.alanp_s3_tf_deployment.bucket
+#  acl = "private"
+#}
 
-  # Default encryption
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+# Server side encryption config
+resource "aws_s3_bucket_server_side_encryption_configuration" "alanp_s3_tf_encryption" {
+  bucket = aws_s3_bucket.alanp_s3_tf_deployment.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
+}
+
+# Versioning config
+resource "aws_s3_bucket_versioning" "alanp_s3_tf_versioning" {
+  bucket = aws_s3_bucket.alanp_s3_tf_deployment.id
+
+    # Enabled versioning to track version of object in same bucket
+  versioning_configuration {
+    status = "Enabled"
   }
 }
